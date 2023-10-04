64246fc4924433109e28e5bca6e1d8f2cb80ac1a
Danil <45513393+Pojerman@users.noreply.github.com>
2023-10-04 09:58:17 +0300

    Add routes, page 404

--- src/pages/sign-in/sign-in.tsx
+++ src/pages/sign-in/sign-in.tsx
@@ -1,38 +1,43 @@
 import React from 'react';
+import Footer from '../../components/footer/footer.tsx';
 
 export default function SignIn(): React.JSX.Element {
   return (
-    <div className="player">
-      <video src="#" className="player__video" poster="img/player-poster.jpg" />
-      <button type="button" className="player__exit">
-        Exit
-      </button>
-      <div className="player__controls">
-        <div className="player__controls-row">
-          <div className="player__time">
-            <progress className="player__progress" value={30} max={100} />
-            <div className="player__toggler" style={{ left: '30%' }}>
-              Toggler
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <div className="logo">
+          <a href="main.html" className="logo__link">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </a>
+        </div>
+
+        <h1 className="page-title user-page__title">Sign in</h1>
+      </header>
+
+      <div className="sign-in user-page__content">
+        <form action="#" className="sign-in__form">
+          <div className="sign-in__fields">
+            <div className="sign-in__field">
+              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email"
+                id="user-email"
+              />
+              <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
+            </div>
+            <div className="sign-in__field">
+              <input className="sign-in__input" type="password" placeholder="Password" name="user-password"
+                id="user-password"
+              />
+              <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
           </div>
-          <div className="player__time-value">1:30:29</div>
-        </div>
-        <div className="player__controls-row">
-          <button type="button" className="player__play">
-            <svg viewBox="0 0 14 21" width={14} height={21}>
-              <use xlinkHref="#pause" />
-            </svg>
-            <span>Pause</span>
-          </button>
-          <div className="player__name">Transpotting</div>
-          <button type="button" className="player__full-screen">
-            <svg viewBox="0 0 27 27" width={27} height={27}>
-              <use xlinkHref="#full-screen" />
-            </svg>
-            <span>Full screen</span>
-          </button>
-        </div>
+          <div className="sign-in__submit">
+            <button className="sign-in__btn" type="submit">Sign in</button>
+          </div>
+        </form>
       </div>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/types/films.tsx
@@ -0,0 +1,5 @@
+export interface FilmsInfo {
+  title: string;
+  genre: string;
+  year: string | number;
+}
